d487bcf23597931c8b3c8ae00d2660472fbd7380
Chris King <kingces95@gmail.com>
2018-09-21 09:41:01 -1000

    clear

--- descriptor/clear/index.js
+++ descriptor/clear/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
-function clear() {
+var create = require('@kingjs/descriptor.create');
+
+function clear(name, copyOnWrite) {
+
+  if (name in this == false)
+    return this;
+
+  if (name in Object.getPrototypeOf(this))
+    copyOnWrite = true;
+
+  var updatedThis = this;
+  if (copyOnWrite || Object.isFrozen(this))
+    updatedThis = create(updatedThis);
+
+  delete updatedThis[name];
+  return updatedThis;
 }
 
 Object.defineProperties(module, {
