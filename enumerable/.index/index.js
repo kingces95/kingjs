45c84fb00ccd0adf4010c9dfb89d4da32559ff19
kingces95 <kingces95@gmail.com>
2019-02-04 21:31:36 -1000

    more cleanup

--- enumerable/.index/index.js
+++
@@ -1,7 +0,0 @@
-Object.defineProperties(module.exports, {
-  create: { value: require('@kingjs/enumerable.create') },
-  define: { value: require('@kingjs/enumerable.define') },
-  forEach: { value: require('@kingjs/enumerable.for-each') },
-  fromEach: { value: require('@kingjs/enumerable.from-each') },
-  odometer: { value: require('@kingjs/enumerable.odometer') },
-});
--- enumerable/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('./.index/test');
-require('./create/test');
-require('./define/test');
-require('./for-each/test');
-require('./from-each/test');
-require('./odometer/test');
