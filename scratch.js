4714429cd506b35939557de119c14081a7b593a4
kingces95 <kingces95@gmail.com>
2019-03-13 12:24:33 -1000

    asdf

--- scratch.js
+++ scratch.js
@@ -1,6 +1,5 @@
 var zlib = require('zlib');
 var assert = require('assert');
-var codes = zlib.codes;
 
 const input = Buffer.from('0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789');
 
@@ -15,41 +14,32 @@ zlib.deflateRaw(input, (err, deflatedBuffer) => {
   var stream = zlib.createInflateRaw();
   var position = 0;
 
-  stream.on('error', e => {
-    // I expect notification the EOF of the deflate stream has been found
-    log('error', e);
-    stream.end();
-  })
-
   stream.on('data', function(chunk) {
     buffers.push(chunk);
     bytesRead += chunk.length;
-    log('decompressed:', 'written', stream.bytesWritten, 'read', bytesRead, 'result', stream.result);
-    if (stream.result == codes.Z_STREAM_END) {
-      log('discarded:', position - stream.bytesWritten);
-      stream.end();
-    }
+    log('decompressed:', 'written', stream.bytesWritten, 'read', bytesRead);
   })
 
-  stream.on('finish', function() {
+  stream.on('end', function done() {
     var result = Buffer.concat(buffers, bytesRead);
     this.close();
     log('result:', result.toString());
     log('chunks:', buffers.length);
     log('bytesWritten:', stream.bytesWritten);
     log('numberRead:', bytesRead);
-  })
+    log('discarded:', position - stream.bytesWritten);
+  });
 
   log('download: started')
   stream.write(deflatedBuffer.slice(0, 5)); position += 5;
   stream.write(deflatedBuffer.slice(5, 10)); position += 5;
   stream.write(deflatedBuffer.slice(10)); position += deflatedBuffer.length - 10;
-  stream.write(Buffer.from([1,2,3,4,5,6])); position += 6;
+  stream.write(Buffer.from([1,2,3,4,5,6])); position += 6; // discarded
 
   log('download: delayed')
   setTimeout(() => {
     log('download: resumed')
-    log(stream.write(Buffer.from([1,2,3,4,5,6]))); // discarded
+    log(stream.write(Buffer.from([1,2,3,4,5,6]))); // rejected
     log('download: finished')
     stream.end();
   }, 1000);
